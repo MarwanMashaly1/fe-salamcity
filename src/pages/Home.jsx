5e4df641f516d06eaa7a9b60dc63f326c37868ca
MarwanMashaly1 <mmash102@uottawa.ca>
2023-08-08 19:54:37 -0400

    add featured events

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Navbar from "../components/Navbar";
 import {
   Box,
   Typography,
@@ -8,62 +7,116 @@ import {
   CardMedia,
   CardActionArea,
 } from "@mui/material";
-import { useStyles } from "@mui/styles";
-import Carousel from "react-material-ui-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 const featuredPosts = [
   {
-    title: "Featured post",
+    title: "Featured post 1",
     description:
       "This is a wider card with supporting text below as a natural lead-in to additional content.",
     image: "https://source.unsplash.com/random",
-    imageLabel: "Image Text",
+    imageLabel: "Image Text 1",
   },
   {
-    title: "Post title",
+    title: "Featured post 2",
     description:
       "This is a wider card with supporting text below as a natural lead-in to additional content.",
     image: "https://source.unsplash.com/random",
-    imageLabel: "Image Text",
+    imageLabel: "Image Text 2",
   },
+  {
+    title: "Featured post 3",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageLabel: "Image Text 2",
+  },
+  {
+    title: "Featured post 4 ",
+    description:
+      "This is a wider card with supporting text below as a natural lead-in to additional content.",
+    image: "https://source.unsplash.com/random",
+    imageLabel: "Image Text 2",
+  },
+  // Add more featuredPosts if needed
 ];
 
+// ... (previous imports and code)
+
 const Home = () => {
+  const pairsOfPosts = [];
+  for (let i = 0; i < featuredPosts.length; i += 2) {
+    pairsOfPosts.push(featuredPosts.slice(i, i + 2));
+  }
+
   return (
     <div className="home">
       <h1>Home</h1>
       <div className="featured">
         <Box
           sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
             width: "100%",
-            height: "100%",
+            minHeight: "100vh",
             bgcolor: "background.paper",
           }}
         >
           <Typography variant="h4" component="div" gutterBottom>
             Featured
           </Typography>
-          <Box sx={{ width: "100%", typography: "body1" }} component="div">
-            <Carousel>
-              {featuredPosts.map((post, index) => (
-                <Card sx={{ maxWidth: 345 }} key={index}>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      height="140"
-                      image={post.image}
-                      alt={post.imageLabel}
-                    />
-                    <CardContent>
-                      <Typography gutterBottom variant="h5" component="div">
-                        {post.title}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {post.description}
-                      </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </Card>
+          <Box
+            sx={{
+              width: "80%", // Adjust the width of the carousel container
+              margin: "0 auto", // Center the carousel container
+              typography: "body1",
+            }}
+            component="div"
+          >
+            <Carousel
+              showThumbs={false}
+              showStatus={false}
+              showArrows={true}
+              infiniteLoop={true}
+              emulateTouch={true}
+              swipeable={true}
+              centerSlidePercentage={50}
+              autoPlay={true}
+            >
+              {pairsOfPosts.map((pair, index) => (
+                <div key={index} className="pair-of-cards">
+                  {pair.map((post, postIndex) => (
+                    <Card
+                      key={postIndex}
+                      sx={{
+                        maxWidth: "100%",
+                        width: "45%", // Adjust the card width for two cards per slide
+                        margin: "10px",
+                        marginBottom: "40px",
+                        display: "inline-block", // Display cards inline
+                      }}
+                    >
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          height="140"
+                          image={post.image}
+                          alt={post.imageLabel}
+                        />
+                        <CardContent>
+                          <Typography gutterBottom variant="h5" component="div">
+                            {post.title}
+                          </Typography>
+                          <Typography variant="body2" color="text.secondary">
+                            {post.description}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </Card>
+                  ))}
+                </div>
               ))}
             </Carousel>
           </Box>
